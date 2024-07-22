6928ae43bea80552777055fae14291b5694da432
ttzytt <Zyt2006613@outlook.com>
2024-07-22 14:08:14 -0400

    add unit tests

--- tests/unit_test.ml
+++ tests/unit_test.ml
@@ -1,13 +1,36 @@
 open Utils
-
-let main () = 
-  let struct_def_str = "
+open Syntax 
+open Parser
+let test_struct_def1 () = 
+  (* with visibility params and fields *)
+  let ipt_str = "
   pub struct Point {
-      x: i32,
-      y: i32,
-  }
-  "  in 
-  (* print_endline struct_def_str;; *)
-  print_endline (Syntax.show_item (parse_ast struct_def_str));;
+    x: i32,
+    y: i32,
+  }" in
+  let expected  = (Vis_Item ([], (Some Pub),
+                             (Struct_vis_item
+                                (Struct
+                                   ("Point", None, None,
+                                    (Some [([], None, "x", "i32"); ([], None, "y", "i32")]))))
+                            )) in
+  Alcotest.(check item_testable) "same item" expected (parse_ast ipt_str);
+  
+  let ipt_str = "
+  pub struct Point {
+    x: i32,
+    y: i32
+  }" in (* should be the same without the comma *)
+  Alcotest.(check item_testable) "same item" expected (parse_ast ipt_str);
 
-main()
+  let ipt_str = "
+  struct Point{
+    x: i32  
+    y: i32
+  }
+  " in (* should raise exception since forgot to add comma *)
+  Alcotest.check_raises "missing comma" (Parser.Error) (fun () -> let _ = parse_ast ipt_str in ())
+
+let () = let open Alcotest in run "unit tests" [
+    "struct-case", [test_case "struct_def1" `Quick test_struct_def1]
+  ]
--- tests/utils.ml
+++ tests/utils.ml
@@ -1,4 +1,8 @@
+open Syntax 
+
 let parse_ast s  = 
   let lexbuf = Lexing.from_string s in
   let ast = Parser.item Lexer.read_token lexbuf in
 ast
+
+let item_testable = Alcotest.testable Syntax.pp_item Syntax.equal_item
